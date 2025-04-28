dac49b8a4ee2f6b000b1447e96f79d29a23fae40
PICHCHIKA BALAJI <109253231+Balajipicchika@users.noreply.github.com>
2025-04-28 23:02:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -265,5 +265,5 @@ def predict():
     return html_output
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=10000)
